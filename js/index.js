e41b2753ec562190ac515ae3e4ab0901e2322b2f
RasmusLD <rasmusld@gmail.com>
2015-03-18 09:24:59 +0100

    updating
    still doesn't differentiate between meeting rooms, the API hasn't been
    made...

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function() {
+        phonegapReady();
+    }
+};
+
+function phonegapReady() {
+
 $(document).ready(function(){
 
 //updates the JSON one time, once the document is ready
@@ -37,33 +64,39 @@ function occupationEvaluater() {
     }
 }
 
-
+//makes sure there will be no "double" updates
+var updating = false;
 //update list function using JQuery mobile
 var updateListJQ = function() {
-    win = document.getElementById("whoIsNext");
-    toBePrinted = "";
-    $.getJSON('http://is2015020209pswc.wp.olo.dk/', function(json){
-        if(json.total_results !== 0) {
-            
-            for(var i = 0; i < json.length; i++) {
-                toBePrinted += "<p>Booked fra: " + json[i].start_tid.substring(8,10) + "/" +
-                json[i].start_tid.substring(5,7) + " " + 
-                json[i].start_tid.substring(11, 16) + " til: " + 
-                json[i].slut_tid.substring(11, 16) + " af: " + 
-                json[i].firmanavn + "</p>";
+    if (!updating) {
+        updating = true;
+        var win = document.getElementById("whoIsNext");
+        var toBePrinted = "";
+        $.getJSON('http://is2015020209pswc.wp.olo.dk/', function(json){
+            if(json.total_results !== 0) {
                 
-                firstStart = json[0].epocstart;
-                firstEnd = json[0].epocend;
-                occupiedOrFree();
-            }
-            $(win).empty();
-            $(win).append(toBePrinted);
-        }else {
-            //failure to get JSON
-            $(win).empty();
-            $(win).append("<p>json not found</p><p>No JSON was returned... Contact an administrator.</p>");
-        }
-    });
+                for(var i = 0; i < json.length; i++) {
+                    toBePrinted += "<p>Booked fra: " + json[i].start_tid.substring(8,10) + "/" +
+                    json[i].start_tid.substring(5,7) + " " + 
+                    json[i].start_tid.substring(11, 16) + " til: " + 
+                    json[i].slut_tid.substring(11, 16) + " af: " + 
+                    json[i].firmanavn + "</p>";
+
+                    firstStart = json[0].epocstart;
+                    firstEnd = json[0].epocend;
+                    occupiedOrFree();
+                }
+                $(win).empty();
+                $(win).append(toBePrinted);
+                updating = false;
+            }else {
+                //failure to get JSON
+                $(win).empty();
+                $(win).append("<p>json not found</p><p>No JSON was returned... Try updating again and give it a moment,<br>if that doesn't work contact an administrator.</p>");
+                updating = false;;
+            };
+        });
+    };
 };
 
 //changes the occupancy state of the freeOrOccupied html element
@@ -82,3 +115,5 @@ butUp = document.getElementById("body_update").addEventListener ("click", update
         updateListJQ();
     }
 });
+
+};
